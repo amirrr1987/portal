a524c5034a4c5cd208f93198b3087e947b9a85f5
Amir Maghami <Maghami.A1987@gmail.com>
2025-01-21 23:41:52 +0330

    Update Item3.vue

--- client/src/pages/Admin/Acceptor/Edit/Item3.vue
+++ client/src/pages/Admin/Acceptor/Edit/Item3.vue
@@ -2,43 +2,16 @@
   <v-row>
     <v-col cols="12">
       <v-row>
-        <ImageUploader
-          :img-src="imageSrc.logo"
-          img-name="logo"
-          md="4"
-          required
-        >
-          <template #header>
-            بارگذاری لوگو
-          </template>
+        <ImageUploader :img-src="imageSrc.logo" img-name="logo" md="4" required>
+          <template #header> بارگذاری لوگو </template>
         </ImageUploader>
       </v-row>
       <v-row>
-        <ImageUploader
-          :img-src="imageSrc.pic1"
-          img-name="pic1"
-          md="4"
-        />
-        <ImageUploader
-          :img-src="imageSrc.pic2"
-          img-name="pic2"
-          md="4"
-        />
-        <ImageUploader
-          :img-src="imageSrc.pic3"
-          img-name="pic3"
-          md="4"
-        />
-        <ImageUploader
-          :img-src="imageSrc.pic4"
-          img-name="pic4"
-          md="4"
-        />
-        <ImageUploader
-          :img-src="imageSrc.pic5"
-          img-name="pic5"
-          md="4"
-        />
+        <ImageUploader :img-src="imageSrc.pic1" img-name="pic1" md="4" />
+        <ImageUploader :img-src="imageSrc.pic2" img-name="pic2" md="4" />
+        <ImageUploader :img-src="imageSrc.pic3" img-name="pic3" md="4" />
+        <ImageUploader :img-src="imageSrc.pic4" img-name="pic4" md="4" />
+        <ImageUploader :img-src="imageSrc.pic5" img-name="pic5" md="4" />
       </v-row>
       <v-row>
         <ImageUploader
@@ -47,9 +20,7 @@
           md="4"
           required
         >
-          <template #header>
-            بارگذاری تصویر کارت ملی
-          </template>
+          <template #header> بارگذاری تصویر کارت ملی </template>
         </ImageUploader>
         <ImageUploader
           :img-src="imageSrc.birthCertificate"
@@ -57,9 +28,7 @@
           md="4"
           required
         >
-          <template #header>
-            بارگذاری تصویر شناسنامه
-          </template>
+          <template #header> بارگذاری تصویر شناسنامه </template>
         </ImageUploader>
         <ImageUploader
           :img-src="imageSrc.businessLicense"
@@ -67,9 +36,7 @@
           md="4"
           required
         >
-          <template #header>
-            بارگذاری پروانه کسب مربوطه
-          </template>
+          <template #header> بارگذاری پروانه کسب مربوطه </template>
         </ImageUploader>
         <ImageUploader
           :img-src="imageSrc.ownershipDocument"
@@ -77,9 +44,7 @@
           md="4"
           required
         >
-          <template #header>
-            بارگذاری اجاره نامه یا سند مالکیت
-          </template>
+          <template #header> بارگذاری اجاره نامه یا سند مالکیت </template>
         </ImageUploader>
         <ImageUploader
           :img-src="imageSrc.bills"
@@ -87,9 +52,7 @@
           md="4"
           required
         >
-          <template #header>
-            بارگذاری قبض (برق، آب و...)
-          </template>
+          <template #header> بارگذاری قبض (برق، آب و...) </template>
         </ImageUploader>
       </v-row>
 
@@ -134,87 +97,53 @@
   </v-row>
 </template>
 
-<script>
+<script setup lang="ts">
+import { onMounted } from "vue";
 import { EventBus } from "@/mixins/EventBus.js";
-
 import ImageUploader from "@/components/ImgUploader";
-export default {
-  name: "Item3",
-  components: {
-    ImageUploader,
-  },
 
-  props: {
-    imageSrc: { type: Object },
-  },
-  data() {
-    return {};
-  },
-  mounted() {
-    EventBus.$on("selectImg", (data) => {
-      this.imageSrc[data.name] = data.src;
-    });
-  },
-  methods: {
-    //////////////////////////////////
-    // Start check form validation
+const props = defineProps({
+  imageSrc: { type: Object, required: true },
+});
 
-    validate() {
-      // if (this.$refs.thisForm.validate()) {
-      this.assign();
-      // }
-    },
-
-    //  End  check form validation
-    //////////////////////////////////
-
-    //////////////////////////////////
-    // Start send data to Parent data
-    assign() {
-      this.$parent.$parent.create = {
-        ...this.$parent.$parent.create,
-        ...this.imageSrc,
-      };
-
-      this.submitForm();
-    },
-    //  End send data to Parent data
-    //////////////////////////////////
-    ////////////////8e24a85b845a40d42c9cdee7d51352679e3ca949//////////////////
-    // Start brn next form
-    btnBack() {
-      this.$parent.$parent.step = 3;
-      window.scrollTo(0, 0);
-    },
-    //  End  brn next form
-    //////////////////////////////////
-
-    //////////////////////////////////
-    // Start send data
-    submitForm() {
-      const formData = new FormData();
-      const oFormData = this.$parent.$parent.create;
-
-      for (const key in oFormData) {
-        if (oFormData[key] != null) formData.append(key, oFormData[key]);
-      }
-      // console.log(
-      //   `I'm here :  ${this.$privateKey}/accepter , ${this.$parent.$parent.create}`
-      // );
-      this.$http
-        .post(`${this.$privateKey}/accepter`, this.$parent.$parent.create)
-        .then(() => {
-          this.dialog = true;
-        })
-        .catch(() => {
-          this.error = true;
-        });
-    },
-    //  End  send data
-    //////////////////////////////////
-  },
+const validate = () => {
+  assign();
 };
+
+const assign = () => {
+  const parent = (parent as any).$parent;
+  parent.create = {
+    ...parent.create,
+    ...props.imageSrc,
+  };
+  submitForm();
+};
+
+const btnBack = () => {
+  const parent = (parent as any).$parent;
+  parent.step = 3;
+  window.scrollTo(0, 0);
+};
+
+const submitForm = async () => {
+  const parent = (parent as any).$parent;
+  try {
+    await fetch(`${import.meta.env.VITE_API_ACCEPTER}`, {
+      method: "POST",
+      body: JSON.stringify(parent.create),
+    });
+    parent.dialog = true;
+  } catch (error) {
+    parent.error = true;
+    console.error(error);
+  }
+};
+
+onMounted(() => {
+  EventBus.on("selectImg", (data) => {
+    props.imageSrc[data.name] = data.src;
+  });
+});
 </script>
 
-<style>
-</style>
+<style></style>
