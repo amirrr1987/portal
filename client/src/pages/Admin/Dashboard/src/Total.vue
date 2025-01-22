18c1427df0935982f2037562ab2235dfaa6602c6
Amir Maghami <Maghami.A1987@gmail.com>
2025-01-22 09:18:29 +0330

    Update Total.vue

--- client/src/pages/Admin/Dashboard/src/Total.vue
+++ client/src/pages/Admin/Dashboard/src/Total.vue
@@ -3,79 +3,103 @@
     <TotalItem
       title="تعداد نمایندگان"
       :total="allData.agent"
-      iconBgColor="#f1c60a"
+      icon-bg-color="#f1c60a"
       md="4"
     >
-      <IconMonth slot="iconName" width="30" height="40" fill="#FFF" />
+      <template #iconName>
+        <IconMonth
+          width="30"
+          height="40"
+          fill="#FFF"
+        />
+      </template>
     </TotalItem>
 
     <TotalItem
       title="تعداد پذیرندگان"
       :total="allData.acceptor"
-      iconBgColor="#08b5dd"
+      icon-bg-color="#08b5dd"
       md="4"
     >
-      <IconMonth slot="iconName" width="30" height="40" fill="#FFF" />
+      <template #iconName>
+        <IconMonth
+          width="30"
+          height="40"
+          fill="#FFF"
+        />
+      </template>
     </TotalItem>
 
     <TotalItem
       title="تعداد تراکنش ها"
       :total="allData.transaction"
-      iconBgColor="#8e329e"
+      icon-bg-color="#8e329e"
       md="4"
     >
-      <IconMonth slot="iconName" width="30" height="40" fill="#FFF" />
+      <template #iconName>
+        <IconMonth
+          width="30"
+          height="40"
+          fill="#FFF"
+        />
+      </template>
     </TotalItem>
 
     <TotalItem
       title="فروش هفتگی"
       :total="allData.weekly"
-      iconBgColor="#32889e"
+      icon-bg-color="#32889e"
       md="4"
     >
-      <IconMonth slot="iconName" width="30" height="40" fill="#FFF" />
+      <template #iconName>
+        <IconMonth
+          width="30"
+          height="40"
+          fill="#FFF"
+        />
+      </template>
     </TotalItem>
 
     <TotalItem
       title="فروش ماهانه"
       :total="allData.weekly"
-      iconBgColor="#00cf90"
+      icon-bg-color="#00cf90"
       md="4"
     >
-      <IconMonth slot="iconName" width="30" height="40" fill="#FFF" />
+      <template #iconName>
+        <IconMonth
+          width="30"
+          height="40"
+          fill="#FFF"
+        />
+      </template>
     </TotalItem>
 
     <TotalItem
       title="موجودی کیف پول (ریال)"
       :total="allData.weekly"
-      iconBgColor="#689e32"
+      icon-bg-color="#689e32"
       md="4"
     >
-      <IconMonth slot="iconName" width="30" height="40" fill="#FFF" />
+      <template #iconName>
+        <IconMonth
+          width="30"
+          height="40"
+          fill="#FFF"
+        />
+      </template>
     </TotalItem>
   </v-row>
 </template>
 
-<script>
-import TotalItem from "@/components/TotalItem";
-import IconMonth from "@/icons/Main/IconTransaction";
-export default {
-  name: "Total",
-  components: {
-    TotalItem,
-    IconMonth,
-  },
-  data() {
-    return {
-      allData: {
-        agent: 1_223_423,
-        acceptor: 1_342_349,
-        transaction: 1_234_234,
-        weekly: 123_422,
-      },
-    };
-  },
-};
+<script setup lang="ts">
+import TotalItem from "@/components/TotalItem.vue";
+import IconMonth from "@/icons/Main/IconTransaction.vue";
+import { ref } from "vue";
+const allData = ref({
+  agent: 1_223_423,
+  acceptor: 1_342_349,
+  transaction: 1_234_234,
+  weekly: 123_422,
+});
 </script>
-
-<style></style>
