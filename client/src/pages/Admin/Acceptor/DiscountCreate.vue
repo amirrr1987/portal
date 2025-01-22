38a1873f0566daf86d8f44398e7330aac139cd30
Amir Maghami <Maghami.A1987@gmail.com>
2025-01-22 08:40:09 +0330

    Update DiscountCreate.vue

--- client/src/pages/Admin/Acceptor/DiscountCreate.vue
+++ client/src/pages/Admin/Acceptor/DiscountCreate.vue
@@ -1,25 +1,14 @@
 <template>
-  <v-form
-    ref="thisForm"
-    lazy-validation
-    @submit.prevent="validateForm"
-  >
+  <v-form ref="thisForm" lazy-validation @submit.prevent="validateForm">
     <v-card class="pa-lg-16">
-      <CardTitle
-        label="ثبت تخفیفات پذیرنده"
-        color="#08b5dd"
-      />
+      <CardTitle label="ثبت تخفیفات پذیرنده" color="#08b5dd" />
       <v-row>
         <AcceptorAutocomplete
           v-model="itemData.accepter"
           label="انتخاب پذیرنده"
           md="4"
         />
-        <Texts
-          v-model="itemData.title"
-          label="نام گروه تخفیف"
-          md="4"
-        />
+        <Texts v-model="itemData.title" label="نام گروه تخفیف" md="4" />
         <Number
           v-model="itemData.discount"
           label="درصد تخفیف گروه"
@@ -33,64 +22,26 @@
         />
       </v-row>
       <v-row class="mt-10 mb-16">
-        <v-btn
-          color="#08b5dd"
-          class="white--text py-6"
-          tile
-          @click="addCard"
-        >
+        <v-btn color="#08b5dd" class="white--text py-6" tile @click="addCard">
           <span>اضافه کردن محصولات یا خدمات</span>
-          <v-icon
-            color="#08b5dd"
-            class="white mr-3"
-            large
-          >
-            mdi-plus
-          </v-icon>
+          <v-icon color="#08b5dd" class="white mr-3" large> mdi-plus </v-icon>
         </v-btn>
       </v-row>
 
-      <v-row
-        v-for="(product, index) in itemData.products"
-        :key="index"
-      >
+      <v-row v-for="(product, index) in itemData.products" :key="index">
         <v-col cols="12">
-          <v-card
-            color="#F1F1F1"
-            class="pa-6 rounded-lg-2 mb-16"
-          >
-            <v-btn
-              icon
-              class="red"
-              x-small
-            >
-              <v-icon
-                color="white"
-                @click="removeCard(index)"
-              >
+          <v-card color="#F1F1F1" class="pa-6 rounded-lg-2 mb-16">
+            <v-btn icon class="red" x-small>
+              <v-icon color="white" @click="removeCard(index)">
                 mdi-close
               </v-icon>
             </v-btn>
             <v-row>
-              <v-col
-                cols="12"
-                md="8"
-              >
+              <v-col cols="12" md="8">
                 <v-row>
-                  <Texts
-                    v-model="product.name"
-                    label="نام"
-                    md="6"
-                  />
-                  <Number
-                    v-model="product.price"
-                    label="قیمت"
-                    md="6"
-                  />
-                  <Texts
-                    v-model="product.description"
-                    label="توضیحات"
-                  />
+                  <Texts v-model="product.name" label="نام" md="6" />
+                  <Number v-model="product.price" label="قیمت" md="6" />
+                  <Texts v-model="product.description" label="توضیحات" />
                 </v-row>
               </v-col>
               <ImageUploader
@@ -104,18 +55,15 @@
       </v-row>
 
       <v-row justify="end">
-        <Btn
-          label="ثبت و تائید"
-          color="#4fc7db"
-          dark
-          type="submit"
-        />
+        <Btn label="ثبت و تائید" color="#4fc7db" dark type="submit" />
       </v-row>
     </v-card>
   </v-form>
 </template>
 
-<script>
+<script setup lang="ts">
+import { ref } from "vue";
+import { useRouter } from "vue-router";
 import Texts from "@/components/Texts";
 import CardTitle from "@/components/CardTitle";
 import Btn from "@/components/Btn";
@@ -124,89 +72,61 @@ import Number from "@/components/Number";
 import AcceptorAutocomplete from "@/components/AcceptorAutocomplete";
 import { EventBus } from "@/mixins/EventBus.js";
 
-export default {
-  name: "Create",
-  components: {
-    Texts,
-    CardTitle,
-    Btn,
-    ImageUploader,
-    Number,
-    AcceptorAutocomplete,
-  },
-  data() {
-    return {
-      itemData: {
-        accepter: "",
-        title: "",
-        discount: 0,
-        products: [],
-      },
-      rules: {
-        persent: (value) => 100 > value > 0 || "این فیلد الزامی است",
-      },
-    };
-  },
-  mounted() {
-    EventBus.$on("selectImg", (data) => {
-      this.itemData.products[data.name].image = data.src;
-    });
-  },
-  methods: {
-    addCard() {
-      this.itemData.products.push({
-        name: "",
-        price: null,
-        description: "",
-        image: "",
-      });
-    },
-    removeCard(index) {
-      this.itemData.products.splice(index, 1);
-    },
+const router = useRouter();
+const thisForm = ref<any>(null);
 
-    //////////////////////////////////
-    // Start check form validation
+const itemData = ref({
+  accepter: "",
+  title: "",
+  discount: 0,
+  products: [],
+});
 
-    validateForm() {
-      if (this.$refs.thisForm.validate()) {
-        this.submitForm();
-      }
-    },
-
-    //  End  check form validation
-    //////////////////////////////////
-
-    //////////////////////////////////
-    // Start send data
-    submitForm() {
-      const formData = new FormData();
-      const oFormData = this.itemData;
-      for (const key in oFormData) {
-        if (oFormData[key] != null) formData.append(key, oFormData[key]);
-      }
-      this.$http
-        .post(process.env.VUE_APP_API_ACCEPTOR_DISCOUNT, this.itemData)
-        .then(() => {
-          this.dialog = true;
-          this.routerToList();
-        })
-        .catch(() => {
-          this.error = true;
-        });
-    },
-    //  End  send data
-    //////////////////////////////////
-
-    //////////////////////////////////
-    // Start router to list
-    routerToList() {
-      setTimeout(() => {
-        this.$router.push(`/admin/acceptor/discount-list`);
-      }, 600);
-    },
-    //  End  router to list
-    //////////////////////////////////
-  },
+const rules = {
+  persent: (value: number) =>
+    (value > 0 && value < 100) || "این فیلد الزامی است",
 };
+
+const addCard = () => {
+  itemData.value.products.push({
+    name: "",
+    price: null,
+    description: "",
+    image: "",
+  });
+};
+
+const removeCard = (index: number) => {
+  itemData.value.products.splice(index, 1);
+};
+
+const validateForm = () => {
+  if (thisForm.value.validate()) {
+    submitForm();
+  }
+};
+
+const submitForm = async () => {
+  try {
+    await fetch(import.meta.env.VITE_API_ACCEPTOR_DISCOUNT, {
+      method: "POST",
+      body: JSON.stringify(itemData.value),
+    });
+    routerToList();
+  } catch (error) {
+    console.error(error);
+  }
+};
+
+const routerToList = () => {
+  setTimeout(() => {
+    router.push("/admin/acceptor/discount-list");
+  }, 600);
+};
+
+onMounted(() => {
+  EventBus.on("selectImg", (data) => {
+    itemData.value.products[data.name].image = data.src;
+  });
+});
 </script>
