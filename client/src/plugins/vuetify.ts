8fbb01478cc7a7551867c177b4f258d08acf79aa
Amir Maghami <Maghami.A1987@gmail.com>
2025-01-22 08:40:00 +0330

    Update vuetify.ts

--- client/src/plugins/vuetify.ts
+++ client/src/plugins/vuetify.ts
@@ -5,45 +5,31 @@
  */
 
 // Styles
-import "@mdi/font/css/materialdesignicons.css";
+import "@mdi/font/scss/materialdesignicons.scss";
 import "vuetify/styles";
 import fa from "vuetify/lib/locale/fa.mjs";
 // Composables
-import { createVuetify } from "vuetify";
+import { createVuetify, type VuetifyOptions } from "vuetify";
 
 // https://vuetifyjs.com/en/introduction/why-vuetify/#feature-guides
 export default createVuetify({
   theme: {
     defaultTheme: "dark",
-    themes: {
-      light: {
-        // primary: "#4fc7db",
-        // secondary: "#0d8dbf",
-        // accent: "#82B1FF",
-        // error: "#FF5252",
-        // info: "#2196F3",
-        // success: "#4CAF50",
-        // warning: "#FFC107",
-      },
+    locale: {
+      rtl: true,
+      locale: "fa",
     },
-    // options: {
-    // customProperties: true,
-    // },
-    // themes: {
-    // light: {
-    // primary: "#4fc7db",
-    // secondary: "#0d8dbf",
-    // accent: "#82B1FF",
-    // error: "#FF5252",
-    // info: "#2196F3",
-    // success: "#4CAF50",
-    // warning: "#FFC107",
-    // },
-    // },
+    themes: {
+      colors: {
+        primary: "#4fc7db",
+        secondary: "#0d8dbf",
+        accent: "#82B1FF",
+        error: "#FF5252",
+        info: "#2196F3",
+        success: "#4CAF50",
+        warning: "#FFC107",
+      },
+      light: {},
+    },
   },
-  // lang: {
-  //   locales: { fa },
-  //   current: "fa",
-  // },
-  // rtl: true,
-});
+} as VuetifyOptions);
