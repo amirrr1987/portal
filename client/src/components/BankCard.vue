7fcb55d9422154d23ec2da93c17d57642dddc6c2
Amir Maghami <Maghami.A1987@gmail.com>
2025-01-22 08:39:39 +0330

    Update BankCard.vue

--- client/src/components/BankCard.vue
+++ client/src/components/BankCard.vue
@@ -1,62 +1,54 @@
 <template>
-  <v-col
-    :cols="cols"
-    :sm="sm"
-    :md="md"
-    :lg="lg"
-    :xl="xl"
-  >
-    <v-card
-      :color="color"
-      class="bank-card text-center"
-    >
-      <v-responsive
-        class="d-flex align-center"
-        :aspect-ratio="16 / 9"
-      >
-        <div class="white--text vira-ltr pb-3">
-          IR{{ sheba }}
-        </div>
+  <v-col v-bind="colProps">
+    <v-card :color="color" class="bank-card text-center">
+      <v-responsive class="d-flex align-center" :aspect-ratio="16 / 9">
+        <div class="white--text vira-ltr pb-3">IR{{ sheba }}</div>
         <div class="white--text">
           {{ label }}
         </div>
-        <v-btn
-          icon
-          class="position-absolute"
-        >
-          <IconDelete
-            width="18"
-            height="18"
-          />
+        <v-btn icon class="position-absolute">
+          <IconDelete width="18" height="18" />
         </v-btn>
       </v-responsive>
     </v-card>
   </v-col>
 </template>
 
-<script lang="ts">
-import IconDelete from "@/icons/Main/IconDelete";
-export default {
-  name: "BankCard",
-  components: {
-    IconDelete,
-  },
-  props: {
-    label: { type: String, default: "بانک گلابی" },
-    sheba: { type: String, default: "------------------" },
-    color: { type: String, default: "black" },
-    cols: { type: String, default: "12" },
-    sm: { type: String, default: null },
-    md: { type: String, default: null },
-    lg: { type: String, default: null },
-    xl: { type: String, default: null },
-  },
-  computed: {},
-};
+<script setup lang="ts">
+import IconDelete from "@/icons/Main/IconDelete.vue";
+
+interface Props {
+  label?: string;
+  sheba?: string;
+  color?: string;
+  cols?: string;
+  sm?: string;
+  md?: string;
+  lg?: string;
+  xl?: string;
+}
+
+const props = withDefaults(defineProps<Props>(), {
+  label: "بانک گلابی",
+  sheba: "------------------",
+  color: "black",
+  cols: "12",
+  sm: "",
+  md: "",
+  lg: "",
+  xl: "",
+});
+
+const colProps = computed(() => ({
+  cols: props.cols,
+  sm: props.sm,
+  md: props.md,
+  lg: props.lg,
+  xl: props.xl,
+}));
 </script>
 
 <style>
-
 .position-absolute {
   position: absolute !important;
   right: 10px;
