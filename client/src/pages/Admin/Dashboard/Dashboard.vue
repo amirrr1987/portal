a2fb7a2d5c8e24c002f9e8c2afc5fa8c199abc00
Amir Maghami <Maghami.A1987@gmail.com>
2025-01-22 09:18:14 +0330

    Update Dashboard.vue

--- client/src/pages/Admin/Dashboard/Dashboard.vue
+++ client/src/pages/Admin/Dashboard/Dashboard.vue
@@ -1,32 +1,21 @@
 <template>
   <div>
     <Total />
-    <Sale class="mt-16"/>
-    <WalletTransaction class="mt-16"/>
+    <Sale class="mt-16" />
+    <WalletTransaction class="mt-16" />
     <LastViolation class="mt-16" />
     <RecentActivity class="mt-16" />
-    <History class="mt-16"/>
+    <History class="mt-16" />
   </div>
 </template>
 
-<script>
-import Total from "@/pages/Admin/Dashboard/src/Total";
-import Sale from "@/pages/Admin/Dashboard/src/Sale";
-import WalletTransaction from "@/pages/Admin/Dashboard/src/WalletTransaction";
-import RecentActivity from "@/pages/Admin/Dashboard/src/RecentActivity";
-import LastViolation from "@/pages/Admin/Dashboard/src/LastViolation"
-import History from "@/pages/Admin/Dashboard/src/ActivityHistory"
-export default {
-  name: "Dashboard",
-  components: {
-    Total,
-    Sale,
-    WalletTransaction,
-    RecentActivity,
-    LastViolation,
-    History,
-  },
-};
+<script setup lang="ts">
+import Total from "@/pages/Admin/Dashboard/src/Total.vue";
+import Sale from "@/pages/Admin/Dashboard/src/Sale.vue";
+import WalletTransaction from "@/pages/Admin/Dashboard/src/WalletTransaction.vue";
+import RecentActivity from "@/pages/Admin/Dashboard/src/RecentActivity.vue";
+import LastViolation from "@/pages/Admin/Dashboard/src/LastViolation.vue";
+import History from "@/pages/Admin/Dashboard/src/ActivityHistory.vue";
 </script>
 
 <style></style>
